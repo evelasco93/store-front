7e982f70e419a3fed366ce5673ee974224ee4308
Edgar Velasco <edgar.velasco93@gmail.com>
2024-09-01 02:27:34 -0600

    feat(server): added basic route for a GET on all products connected to the db via api

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,15 +1,22 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 import cors from 'cors'
+import { PrismaClient } from '@store-front-typescript-bootcamp/db'
 
 const app = express()
+const prisma = new PrismaClient()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/', (_, res) => {
-  return res.json({ ok: true })
+app.get('/', async (_, res) => {
+  const result = await prisma.product.findMany({
+    include: {
+      variants: true,
+    },
+  })
+  return res.json({ result })
 })
 
 const port = process.env.PORT || 5001
